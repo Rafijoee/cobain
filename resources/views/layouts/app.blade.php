3bc51267482b14a70b1167b9c528c0061fb44a82
Rafijoee <rafijoe44@gmail.com>
2024-03-20 06:43:42 +0700

    revisi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,9 +21,6 @@
     <header>
         <nav class="fixed w-full z-20 top-0 left-0 bg-gradient-to-b from-[#000000B2] to-transparent addBlur border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:border-gray-200 dark:bg-gray-900 p-0 m-0 mb-5 navbar-solid-bg" ;>
             <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="{{url('/login')}}" class="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src="{{asset('images/logo_dinas.png')}}" class="h-8" alt="Dinas Pertanian dan Ketahanan Pangan" style="height: 50px" />
-                </a>
                 <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                     <a href="{{url('/login')}}" type="button" class="text-white border-indigo-700 bg-indigo-700 hover:bg-indigo-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800">Login</a>
                     <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
